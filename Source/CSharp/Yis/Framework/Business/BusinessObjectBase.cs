956e55d80b40fbd07dc6feebe1d46ca4d044918f
jeromeb85 <jerome.boisseau@orange.fr>
2014-09-24 17:49:46 +0200

    bog

---
+++ Source/CSharp/Yis/Framework/Business/BusinessObjectBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yis.Framework.Data.Contract;
+using Yis.Framework.Model.Contract;
+
+namespace Yis.Framework.Business
+{
+    public abstract class BusinessObjectBase<TModel, TProvider, TDataContext> : BusinessComponentBase<TModel, TProvider, TDataContext>
+        where TProvider : IRepository<TModel>
+        where TModel : class,IModel
+        where TDataContext : IDataContext
+    {
+        #region Constructors
+
+        public BusinessObjectBase()
+            : base()
+        {
+        }
+
+        #endregion Constructors
+    }
+}
