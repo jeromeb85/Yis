1796b3f0d88dac031a5e5120b3fcebe2bd0c171b
jeromeb85 <jerome.boisseau@orange.fr>
2015-06-26 18:04:57 +0200

    s

---
+++ Source/CSharp/Yis/Erp/Mdm/Data/Memory/ClientProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yis.Erp.Mdm.Data.Contract;
+using Yis.Framework.Data.Contract;
+using Yis.Framework.Data.Memory;
+
+namespace Yis.Erp.Mdm.Data.Memory
+{
+    public class ClientProvider  : RepositoryBase<Model.Client>, IClientProvider
+    {
+
+        public ClientProvider(IDataContext dataContext)
+            : base(dataContext)
+        {
+        }
+
+        public Model.Client GetById(Guid id)
+        {
+            return GetQuery().First(t => t.Id == id);
+        }
+
+    }
+}
---
+++ Source/CSharp/Yis/Erp/Mdm/Data/Memory/MdmDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yis.Erp.Mdm.Data.Contract;
+using Yis.Framework.Data.Memory;
+
+namespace Yis.Erp.Mdm.Data.Memory
+{
+    public class MdmDataContext : DataContextBase, IMdmDataContext
+    {
+        #region Constructors
+
+        public MdmDataContext()
+            : base(@"c:\Data\Erp\")
+        {
+        }
+
+        #endregion Constructors
+    
+    }
+}
